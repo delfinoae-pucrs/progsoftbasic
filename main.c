3a2e85017c61f44e6eb719b82bb4ac490aae4425
unknown <12205117@W1818004.portoalegre.pucrsnet.br>
2016-03-11 18:34:23 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int valor1, valor2;
+  printf("Digite os dois valores: ");
+  scanf("%d %d", &valor1, &valor2);
+  printf("Você digitou: %d e %d\n", valor1, valor2);
+
+  return 0;
+}
